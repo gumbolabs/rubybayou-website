b09de3072e34f8ef0c25c0532d43f6b1f2bddc8d
James W. Thompson, II <james@plainprograms.com>
2009-08-01 22:45:51 -0500

    Refactored PrettyStuff helper to increase code coverage.

--- helper/pretty_stuff.rb
+++ helper/pretty_stuff.rb
@@ -5,22 +5,15 @@ module Ramaze
         start_date = Time.new
         delta_minutes = (start_date.to_i - time.to_i).floor / 60
         
-        case
-          when delta_minutes < 1
-            "less than a minute ago"
-          when delta_minutes < 50
-            "#{delta_minutes} minutes ago"
-          when delta_minutes < 90
-            "about one hour ago"
-          when delta_minutes < 1080
-            "#{(delta_minutes / 60).round} hours ago"
-          when delta_minutes < 1440
-            "one day ago"
-          when delta_minutes < 2880
-            "about one day ago"
-          else
-            "#{(delta_minutes / 1440).round} days ago"
-        end  
+        case delta_minutes
+          when 0..1 then "less than a minute ago"
+          when 2..50 then "#{delta_minutes} minutes ago"
+          when 51..90 then "about one hour ago"
+          when 91..1080 then "#{(delta_minutes / 60).round} hours ago"
+          when 1081..1440 then "one day ago"
+          when 1441..2879 then "about one day ago"
+          else "#{(delta_minutes / 1440).round} days ago"
+        end
       end
     end
   end

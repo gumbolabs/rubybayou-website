367a365f3c28269914e69cc43164489049d5d6e2
James W. Thompson, II <james@plainprograms.com>
2009-08-01 19:02:47 -0500

    Refactored tests to use nested contexts for better readability and such.

--- spec/controller/main.rb
+++ spec/controller/main.rb
@@ -3,27 +3,29 @@ require File.expand_path(File.dirname(__FILE__) + '/../common')
 include Ramaze::Helper::PrettyStuff
 
 describe MainController do
-  behaves_like :rack_test
-
-  it 'home page should respond successfully' do
-    get('/').status.should == 200
-  end
-  
-  it 'home page should include latest blog post' do
-    get ('/')
-    last_response.should.match /<div.*id=\"latest_blog\".*>.*<\/div>/m
-  end
-  
-  it 'home page should include Twitter feed' do
-    get ('/')
-    last_response.should.match /<div.*id=\"twitter\".*>.*<\/div>/m
-  end
+  describe "home page" do
+    behaves_like :rack_test
     
-  it 'home page Twitter feed should include how old the tweets are' do
-    get ('/')
-    @tweets = TwitterCache.get_tweets
-    @tweets.each do |tweet|
-      last_response.should.match /<li>#{Regexp.quote(tweet.text)} <small>\(#{time_ago(tweet.time)}\)<\/small><\/li>/m
+    should "respond successfully" do
+      get('/').status.should == 200
+    end
+  
+    should "include latest blog post" do
+      get ('/')
+      last_response.should.match /<div.*id=\"latest_blog\".*>.*<\/div>/m
+    end
+  
+    should "include Twitter feed" do
+      get ('/')
+      last_response.should.match /<div.*id=\"twitter\".*>.*<\/div>/m
+    end
+    
+    it "Twitter feed should include how old the tweets are" do
+      get ('/')
+      @tweets = TwitterCache.get_tweets
+      @tweets.each do |tweet|
+        last_response.should.match /<li>#{Regexp.quote(tweet.text)} <small>\(#{time_ago(tweet.time)}\)<\/small><\/li>/m
+      end
     end
   end
 end
--- spec/model/twitter_cache.rb
+++ spec/model/twitter_cache.rb
@@ -1,48 +1,56 @@
 require File.expand_path(File.dirname(__FILE__) + '/../common')
 
 describe TwitterCache do
-  should 'respond to time()' do
-    TwitterCache.first.should.respond_to? :time
-  end
-  
-  it "time() should return a Time object" do
-    TwitterCache.first.time.should.be.kind_of? Time
-  end
-  
-  should 'respond to self.update_cache()' do
-    TwitterCache.should.respond_to? :update_cache
-  end
-  
-  should 'have exactly three records after an self.update_cache()' do
-    TwitterCache.update_cache
-    TwitterCache.all.size.should.eql? 3
-  end
-  
   should 'store the text of tweets' do
     TwitterCache.first.should.respond_to? :text
     TwitterCache.first.text.should.not.be.nil?
+    TwitterCache.first.text.should.be.kind_of? String
   end
   
   should 'store the date of tweets' do
     TwitterCache.first.should.respond_to? :date
     TwitterCache.first.date.should.not.be.nil?
+    TwitterCache.first.date.should.be.kind_of? DateTime
   end
   
-  should 'respond to self.get_tweets()' do
-    TwitterCache.should.respond_to? :get_tweets
+  describe "time()" do
+    should "respond" do
+      TwitterCache.first.should.respond_to? :time
+    end
+  
+    should "return a Time object" do
+      TwitterCache.first.time.should.be.kind_of? Time
+    end
   end
   
-  it "self.get_tweets() should return an array of tweets" do
-    TwitterCache.get_tweets.should.be.kind_of? Array
+  describe "self.update_cache()" do
+    should "respond" do
+      TwitterCache.should.respond_to? :update_cache
+    end
+  
+    should "after call there should be exactly three records" do
+      TwitterCache.update_cache
+      TwitterCache.all.size.should.eql? 3
+    end
   end
   
-  it "self.get_tweets() should return exactly 3 tweets" do
-    TwitterCache.get_tweets.size.should.eql? 3
-  end
+  describe "self.get_tweets()" do
+    should "respond" do
+      TwitterCache.should.respond_to? :get_tweets
+    end
   
-  it 'self.get_tweets() should return tweets in descending order' do
-    @tweets = TwitterCache.get_tweets
-    @tweets[0].date.should.be > @tweets[1].date
-    @tweets[1].date.should.be > @tweets[2].date
+    should "return an array of tweets" do
+      TwitterCache.get_tweets.should.be.kind_of? Array
+    end
+  
+    should "return exactly 3 tweets" do
+      TwitterCache.get_tweets.size.should.eql? 3
+    end
+  
+    should "return tweets in descending order" do
+      @tweets = TwitterCache.get_tweets
+      @tweets[0].date.should.be > @tweets[1].date
+      @tweets[1].date.should.be > @tweets[2].date
+    end
   end
 end

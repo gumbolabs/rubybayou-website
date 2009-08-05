92368e3bfc62f7f0bc571d9019b78e13b444f2f1
James W. Thompson, II <james@plainprograms.com>
2009-08-05 14:13:30 -0500

    Implemented New Relic RPM gem.

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "twitter", :version => '0.6.13'
+  config.gem "new_relic", :version => '2.9.4'
+  config.gem "twitter",   :version => '0.6.13'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

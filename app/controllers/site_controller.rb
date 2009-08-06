72969494f339f983fe586dbd4f5c98ca127b3f37
James W. Thompson, II <james@plainprograms.com>
2009-08-05 19:44:02 -0500

    Added 15 minute cache-control directive to SiteController#index.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,7 @@
 class SiteController < ApplicationController
   def index
+    response.headers['Cache-Control'] = 'public, max-age=900'
+    
     @page_title = "New Orleans Ruby User Group"
     @tweets ||= TwitterCache.get_tweets
   end

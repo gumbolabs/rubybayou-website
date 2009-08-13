0c56fa3e1c305986babfce7cb7215366c476841b
James Thompson <james@rubybayou.com>
2009-08-13 00:03:03 -0500

    Enhanced SiteController's specs and achieved 100% C0 code coverage.

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -1,10 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SiteController do
-
-  #Delete this example and add some real ones
-  it "should use SiteController" do
-    controller.should be_an_instance_of(SiteController)
+  context "index action" do
+    before(:each) do
+      get :index
+    end
+    
+    it "should respond" do
+      controller.should.respond_to? :index
+    end
+    
+    it "should set @title" do 
+      assigns[:page_title].should_not be_nil
+      assigns[:page_title].should == "New Orleans Ruby User Group"
+    end
+    
+    it "should set @tweets" do
+      assigns[:tweets].should_not be_nil
+      assigns[:tweets].size.should == 3
+    end
+    
+    it "should set Cache-Control header to 900 seconds" do
+      response.headers['Cache-Control'].should == 'public, max-age=900'
+    end
   end
-
 end

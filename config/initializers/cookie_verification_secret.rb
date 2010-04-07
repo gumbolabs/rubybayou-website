98c7cbf01a4482e3874af51918b21c1d4ed440ba
James Thompson <james@rubybayou.com>
2010-04-07 15:09:04 -0500

    Initial updgrade of site to Rails 3.

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.cookie_secret = 'f70299e060f5ebfd510d383ddc23befe6e26625338fdd947fcb8411cf564596b78fe6fc06bbf9c967753318db324cd4aabc572c1ef4c7d1b6f1d11131c584371'
